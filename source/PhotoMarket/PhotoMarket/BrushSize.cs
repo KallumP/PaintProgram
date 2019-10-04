5a7330e92ed3b0f1ca96e811ad377468e234dd91
Kallum P <kallum179@gmail.com>
2019-10-05 00:32:29 +0100

    Brush Window crashing when too big brush size fixed

--- source/PhotoMarket/PhotoMarket/BrushSize.cs
+++ source/PhotoMarket/PhotoMarket/BrushSize.cs
@@ -19,7 +19,8 @@ namespace PhotoMarket
         int inputTime;
 
         //constructor
-        public BrushSize(MainWindow m) {
+        public BrushSize(MainWindow m)
+        {
 
             InitializeComponent();
 
@@ -30,19 +31,30 @@ namespace PhotoMarket
             newSize = main.penWidth;
 
             //updates the window to show the current brush size
-            brushSize_bar.Value = newSize;
-            newSize_txt.Text = newSize.ToString(); ;
+            newSize_txt.Text = newSize.ToString();
+
+            //makes sure that the curernt size does not go past the maximum value of the scroll bar
+            if (newSize > brushSize_bar.Maximum)
+                brushSize_bar.Value = brushSize_bar.Maximum;
+            else
+                brushSize_bar.Value = newSize;
+            
         }
 
         //deals with the scroll bar
-        private void BrushSize_bar_Scroll(object sender, EventArgs e) {
+        private void BrushSize_bar_Scroll(object sender, EventArgs e)
+        {
 
             //updates the text box to show what brush size was selected
             newSize_txt.Text = brushSize_bar.Value.ToString();
+
+            //sets the new size of the brush
+            newSize = brushSize_bar.Value;
         }
 
         //starts a timer as soon as a key button is let go of
-        private void NewSize_txt_KeyUp(object sender, KeyEventArgs e) {
+        private void NewSize_txt_KeyUp(object sender, KeyEventArgs e)
+        {
 
             //resets the time since last keypress
             inputTime = 0;
@@ -51,13 +63,15 @@ namespace PhotoMarket
             inputTimer.Start();
         }
 
-        private void InputTimer_Tick(object sender, EventArgs e) {
+        private void InputTimer_Tick(object sender, EventArgs e)
+        {
 
             //keeps track of how much time has passed since the last keypress
             inputTime += inputTimer.Interval;
 
             //if the user has stopped typing it sets the new size
-            if (inputTime >= inputTimeout) {
+            if (inputTime >= inputTimeout)
+            {
                 UpdateTextBrushSize();
 
                 inputTimer.Stop();
@@ -65,7 +79,8 @@ namespace PhotoMarket
         }
 
         //sets the new size from the text box value
-        void UpdateTextBrushSize() {
+        void UpdateTextBrushSize()
+        {
 
             int textValue;
 
@@ -73,7 +88,8 @@ namespace PhotoMarket
             Int32.TryParse(newSize_txt.Text, out textValue);
 
             //makes sure that a valid number was entered
-            if (textValue > 0) {
+            if (textValue > 0)
+            {
 
                 newSize = textValue;
 
@@ -83,33 +99,37 @@ namespace PhotoMarket
                 else
                     brushSize_bar.Value = newSize;
 
-            } else
+            }
+            else
+            {
 
                 //reverts the change if an invalid change was made
                 newSize_txt.Text = newSize.ToString();
+            }
         }
 
-
         //lets the user go back without making changes
-        private void Cancel_btn_Click(object sender, EventArgs e) {
+        private void Cancel_btn_Click(object sender, EventArgs e)
+        {
 
             //closes the window without changing the size in the main window
             Close();
         }
 
         //lets the user select the new size
-        private void Select_btn_Click(object sender, EventArgs e) {
+        private void Select_btn_Click(object sender, EventArgs e)
+        {
+            //Lets the user click select before the timer registers that they have stopped typing
+            UpdateTextBrushSize();
 
             //changes the size in the main window
             main.penWidth = newSize;
 
-
+            //updates the pen in the main window
             main.UpdateGlobalPen(main.penColor, newSize);
 
             //closes the window
             Close();
         }
-
-
     }
 }
